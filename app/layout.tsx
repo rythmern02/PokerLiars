d0adc653c8daf7bff2e6522e13f6e06bbb9fe33f
rythmern02 <117756525+rythmern02@users.noreply.github.com>
2024-12-14 22:44:19 +0530

    updating the project and now it is funckin working....

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { GlobalContextProvider } from "./context/GlobalContext";
+import { BrowserRouter } from "react-router-dom";
+import {Sidebar} from "./components/Sidebar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Liars Poker",
+  description: "by Rythme Nagrani",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <GlobalContextProvider>
+          <div className="flex">
+            <Sidebar />
+            <main className="flex-1">
+              {children}
+            </main>
+          </div>
+        </GlobalContextProvider>
+      </body>
+    </html>
+  );
+}
